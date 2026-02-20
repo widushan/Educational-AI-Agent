80cac6695524a1e559aac80df517d17cee721c4e
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-02-20 19:01:35 +0530

    inngest commit
    Start inngesting

--- ai-career-coach/middleware.tsx
+++ ai-career-coach/middleware.tsx
@@ -3,7 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
-    '/'
+    '/',
+    '/api/inngest(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
